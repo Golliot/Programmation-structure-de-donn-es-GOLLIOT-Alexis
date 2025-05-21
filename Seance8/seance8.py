82b40a7e7774fc01c5318b26358feac2915940b4
Golliot <alexis.golliot.sakala@gmail.com>
2025-05-21 17:04:21 +0200

    TD seance 8

--- Seance8/seance8.py
+++ Seance8/seance8.py
@@ -11,31 +11,91 @@ def extract_data():
         res.append(st.unpack_from('hh', data, 44+4*i))
     return res
 
-def create_wav():
-    f = open('new.wav', 'wb')
+def question2():
+    with open('question2.wav', 'wb') as f:
     
-    size = 10786332
-    riff = st.pack("<4sI4s", b'RIFF', size, b'WAVE')
-    fmt = st.pack('<4sIHHIIHH', b'fmt ', 16, 1, 2, 44100 , 176400, 4, 16)
+        size = 10786332
+        riff = st.pack("<4sI4s", b'RIFF', size, b'WAVE')
+        fmt = st.pack('<4sIHHIIHH', b'fmt ', 16, 1, 2, 44100 , 176400, 4, 16)
+        
+        #create header
+        data_header = st.pack('<4sI', b'data', 10786296)
+        f.write(riff)
+        f.write(fmt)
+        f.write(data_header)
+
+        #create data
+        unpacked_data = extract_data()
+        data = b''.join(st.pack('<hh', unpacked_data[i][0], 
+                                unpacked_data[i][1]) for i in range(len(unpacked_data)))
+        f.write(data)
     
-    #create header
-    data_header = st.pack('<4sI', b'data', 10786296)
-    f.write(riff)
-    f.write(fmt)
-    f.write(data_header)
 
-    #create data
-    unpacked_data = extract_data()
-    data = b''.join(st.pack('<hh', unpacked_data[i][0], unpacked_data[i][1]) for i in range(len(unpacked_data)))
-    f.write(data)
+def question3():
+    with open('question3.wav', 'wb') as f:
+
+        size = 5393184
+        
+        #create header
+        riff = st.pack("<4sI4s", b'RIFF', size, b'WAVE')
+        fmt = st.pack('<4sIHHIIHH', b'fmt ', 16, 1, 2, 44100 , 176400, 4, 16)
+        data_header = st.pack('<4sI', b'data', 10786296//2)
+
+        f.write(riff)
+        f.write(fmt)
+        f.write(data_header)
+
+        #create data
+        unpacked_data = extract_data()
+        data = b''.join(st.pack('<hh', unpacked_data[2*i][0],
+                                unpacked_data[2*i][1]) for i in range(len(unpacked_data)//2))
+        f.write(data)
+
+def question4():
+    with open('question4.wav', 'wb') as f:
+        size = 21572628
+        riff = st.pack("<4sI4s", b'RIFF', size, b'WAVE')
+        fmt = st.pack('<4sIHHIIHH', b'fmt ', 16, 1, 2, 44100 , 176400, 4, 16)
+        data_header = st.pack('<4sI', b'data', 10786296*2)
+        f.write(riff)
+        f.write(fmt)
+        f.write(data_header)
+
+        unpacked_data = extract_data()
+        samples = []
+        for i in range(len(unpacked_data)-1):
+            samples.append(st.pack('<hh', unpacked_data[i][0], unpacked_data[i][1]))
+            samples.append(st.pack('<hh',
+                            (unpacked_data[i][0] + unpacked_data[i+1][0])//2,
+                            (unpacked_data[i][1] + unpacked_data[i+1][1])//2))
+        samples.append(st.pack('<hh', unpacked_data[-1][0], unpacked_data[-1][1]))
+        data = b''.join(samples)
+        f.write(data)
     
+def question5(phi: float):
+    with open('question5.wav', 'wb') as f:
+    
+        size = 10786332
+        riff = st.pack("<4sI4s", b'RIFF', size, b'WAVE')
+        fmt = st.pack('<4sIHHIIHH', b'fmt ', 16, 1, 2, int(phi*44100) , 176400, 4, 16)
+        
+        #create header
+        data_header = st.pack('<4sI', b'data', 10786296)
+        f.write(riff)
+        f.write(fmt)
+        f.write(data_header)
+
+        #create data
+        unpacked_data = extract_data()
+        data = b''.join(st.pack('<hh', unpacked_data[i][0], 
+                                unpacked_data[i][1]) for i in range(len(unpacked_data)))
+        f.write(data)
+        
     
 
-create_wav()
-f=open("new.wav", 'rb')
-data = f.read()
-print(data[0:44])
-
-
-
     
+if __name__ == '__main__':
+    question2()
+    question3() #le son est accéléré *2
+    question4() #le son est ralenti *2
+    question5(1.33)
